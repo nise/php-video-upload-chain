837b129a3ee0da8dd790ed9d6abf22c2a1445619
nise <niels.seidel@nise81.com>
2018-04-17 13:30:47 +0200

    enabled webm

--- upload.php
+++ upload.php
@@ -214,20 +214,18 @@ class Upload {
        
         $formatx264 = new FFMpeg\Format\Video\X264();
         $formatx264->setAudioCodec("libmp3lame"); // libvorbis  libmp3lame libfaac
-        //$formatwebm = new FFMpeg\Format\Video\WebM();
-        //$formatwebm->setAudioCodec("libmp3lame");
+        $formatwebm = new FFMpeg\Format\Video\WebM();
+        $formatwebm->setAudioCodec("libmp3lame");
         $formatx264->on('progress', function ($audio, $format, $percentage) {
             //echo "$percentage % transcoded";
         });
         
         $video
-            ->save($formatx264, $this->TMP_DIR . '/' . $name . '.mp4');
-            //->save(new FFMpeg\Format\Video\WMV(), 'export-wmv.wmv')
-            //->save($formatwebm, $this->TMP_DIR . '/export-webm.webm');
+            ->save($formatx264, $this->TMP_DIR . '/' . $name . '.mp4')
+            ->save($formatwebm, $this->TMP_DIR . '/' . $name . '.webm');
 
         // bug: https://github.com/PHP-FFMpeg/PHP-FFMpeg/issues/453
         //$video->filters()->extractMultipleFrames(FFMpeg\Filters\Video\ExtractMultipleFramesFilter::FRAMERATE_EVERY_10SEC, $stillstarget.'test/')->synchronize()->save(new FFMpeg\Format\Video\X264(), 'new.jpg');
-        
         
         return;
     }
