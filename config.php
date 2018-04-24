f511e3e02033a462817c7c9391718569012300c1
nise <niels.seidel@nise81.com>
2018-04-24 10:36:05 +0200

    Added config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+ 'host'=> 'http://'.$_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'],
+ 'host_path'=> $_SERVER['DOCUMENT_ROOT'] . "/videos/test2",
+ 'upload_dir'=> $_SERVER['DOCUMENT_ROOT'] . '/videos',
+ 'tmp_dir'=> $_SERVER['DOCUMENT_ROOT'] . '/videos/tmp',
+ 'stills_dir'=> $_SERVER['DOCUMENT_ROOT'] . '/moodle/mod/videodatabase/images/stills/'
+);
+?>
