0aa5ba7c003540eba611f826bf35c6fecd61c7fb
nise <niels.seidel@nise81.com>
2018-05-19 16:45:10 +0200

    changed path

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return array(
  'host_path' => $_SERVER['DOCUMENT_ROOT'] . "/videos/test2",
  'upload_dir' => $_SERVER['DOCUMENT_ROOT'] . '/videos',
  'tmp_dir' => $_SERVER['DOCUMENT_ROOT'] . '/videos/tmp',
- 'tmp_rel_dir' => '/videos/tmp',
+ 'tmp_rel_dir' => 'http://'.$_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'] . '/videos/tmp',
  'stills_dir' => $_SERVER['DOCUMENT_ROOT'] . '/videos/tmp', // '/moodle/mod/videodatabase/images/stills/',
  'ffmpeg' => '/usr/bin/ffmpeg',
  'ffprobe' => '/usr/bin/ffprobe'
